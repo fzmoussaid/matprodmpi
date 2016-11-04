2162f18f5018f66b30957f5358be9c658836713a
Fatima Zahra Moussaid <fatima.zahra.moussaid94@gmail.com>
2016-11-04 16:30:13 +0100

    Grid Creation

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <math.h>
+#include <assert.h>
+
 
 #include "cmdline.h"
 #include "error.h"
@@ -17,11 +20,31 @@ int main(int argc, char *argv[])
         matprod_error("No input file!\n");
         exit(EXIT_FAILURE);
     }
+    double *mat;
     MPI_Init(NULL, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &group_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    double Nd = sqrt(group_size);
+    assert(floor(Nd) == Nd);
+    int N = (int)Nd;
     printf("#rank: %d; group_size: %d\n", rank, group_size);
+    
+    MPI_Comm cart, line, column;
+    int dims[2] = {N, N};
+    int periods[2] = {0, 1};
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 1, &cart);
+
+    int rank_line, rank_column;
+    int rem_dims[2] = {1, 0};
+    MPI_Cart_sub(cart, rem_dims, &line);
+    MPI_Comm_rank(line, &rank_line);
+
+    int rem_dims2[2] = {0, 1};
+    MPI_Cart_sub(cart, rem_dims2, &column);
+    MPI_Comm_rank(column, &rank_column);
+
+    printf("%d %d %d\n",rank,rank_line, rank_column);
 
     perf_t p1, p2;
     perf(&p1);
