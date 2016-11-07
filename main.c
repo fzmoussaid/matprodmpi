c069c4f10665537bc56f15c72b08cc433bdb60ca
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2016-11-07 20:57:50 +0100

    add random matrix generator

--- main.c
+++ main.c
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
     fox_algorithm(&p, &eq);
     time_end();
 
-    // print result
+    tdp_matrix_print(eq.n, eq.n, eq.C, eq.n, stdout);
     matprod_equation_free(&eq);
 
     print_time(p.rank);
---
+++ matgen/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+
+#include "cmdline.h"
+#include "error.h"
+#include "../util.h"
+
+int main(int argc, char *argv[])
+{
+    srand((long) &argc + time(NULL));
+    
+    struct gengetopt_args_info opt;
+
+    cmdline_parser(argc, argv, &opt);
+    int n = opt.size_arg;
+    
+    double *M = tdp_matrix_new(n, n);
+    tdp_matrix_rand(n, n, M, opt.min_value_arg, opt.max_value_arg);
+    
+    printf("%d\n", n);
+    tdp_matrix_print(n, n, M, n, stdout);
+    free(M);
+
+    return EXIT_SUCCESS;
+}
